89795b30a0299022224e02f2b1501651b90292a4
Oleh Shatov <lexusshatov2@gmail.com>
2022-08-22 16:09:01 +0300

    Add Jetpack Compose

---
+++ app/src/main/java/com/mouse/numberfact/MainScreen.kt
@@ -0,0 +1,9 @@
+package com.mouse.numberfact
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+    Text(text = "Hello world!")
+}
